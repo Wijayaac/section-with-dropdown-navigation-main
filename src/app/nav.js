5fdacbb768830244bbcc19cd6a0f32a753191c9e
Wijayaac <wijayaac25@gmail.com>
2022-05-23 05:40:14 +0800

    Fix : spacing, sizing mobile navigation

--- src/app/nav.js
+++ src/app/nav.js
@@ -4,17 +4,20 @@ class HeaderNavigation {
   }
 
   MobileToggle() {
-    const primaryNav = document.querySelector("#primary-navigation");
-    const navToggle = document.querySelector(".header__toggle.header--mobile");
-
+    const primaryNav = document.querySelector("#primary-navigation"),
+      navToggle = document.querySelector(".header__toggle.header--mobile"),
+      bodyElm = document.body;
+    console.log(bodyElm);
     navToggle.addEventListener("click", () => {
       const visibility = primaryNav.getAttribute("data-visible");
       if (visibility === "false") {
         primaryNav.setAttribute("data-visible", true);
         navToggle.setAttribute("aria-expanded", true);
+        bodyElm.classList.toggle("body--overlay");
       } else {
         primaryNav.setAttribute("data-visible", false);
         navToggle.setAttribute("aria-expanded", false);
+        bodyElm.classList.toggle("body--overlay");
       }
     });
   }
